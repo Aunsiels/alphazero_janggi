42b0a25dfc76e439891b921a62fd55f77a04071c
Julien <romerojulien34@gmail.com>
2020-11-17 15:36:30 +0100

    decorrelate is_check from get_actions

--- ia/tests/test_ia.py
+++ ia/tests/test_ia.py
@@ -9,14 +9,14 @@ from janggi.utils import Color
 
 class TestIA(unittest.TestCase):
 
-    def _test_mcts_vs_random(self):
+    def test_mcts_vs_random(self):
         player_blue = RandomPlayer(Color.BLUE)
-        player_red = RandomMCTSPlayer(Color.RED, n_simulations=800, temperature_start=0.01, temperature_threshold=30, temperature_end=0.01)
+        player_red = RandomMCTSPlayer(Color.RED, n_simulations=200, temperature_start=0.01, temperature_threshold=30, temperature_end=0.01)
         winner = fight(player_blue, player_red, 200)
         self.assertEqual(winner, Color.RED)
 
     def test_random_vs_random(self):
-        n_simulations = 200
+        n_simulations = 1600
         player_blue = RandomMCTSPlayer(Color.BLUE, n_simulations=n_simulations)
         player_red = RandomMCTSPlayer(Color.RED, n_simulations=n_simulations)
         winner = fight(player_blue, player_red, 200)
