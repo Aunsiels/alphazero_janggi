ae6706ccf94bd1006cc35350ed3e86c6e0d648c0
Julien <romerojulien34@gmail.com>
2020-11-27 10:46:01 +0100

    save episodes and reload model more often

--- game_generation_processes.py
+++ game_generation_processes.py
@@ -7,24 +7,36 @@ from multiprocessing import current_process
 
 import torch
 import multiprocessing as mp
-logger = mp.log_to_stderr()
-logger.setLevel(logging.INFO)
 
 from ia.janggi_network import JanggiNetwork
 from ia.trainer import ModelSaver, run_episode_independant
 from janggi.utils import BOARD_HEIGHT, BOARD_WIDTH, DEVICE
 
+# Activate some logs, can be turned off
+logger = mp.log_to_stderr()
+logger.setLevel(logging.INFO)
+
+# Number of residual layers in the network
 N_RESIDUAL = 20
 
-N_POOLS = 2
-N_SIMULATIONS = 400
+# Number of parallel simulations
+N_POOLS = 25
+# Number of simulation per round
+N_SIMULATIONS = 800
+# Number of round per game
 ITER_MAX = 200
-N_EPISODES = 100
-N_ITER_EPISODE = 10
+# Number of episodes between two saving and model reload
+N_EPISODES = N_POOLS
 
+
+# Parameters for parallel processing
+# Maximum batch size processed at once
 BATCH_SIZE = 16
+# Number of features for the board
 N_FEATURES = 16
+# Number of features to represent a policy
 N_FEATURES_POLICY = 58
+# Number of buffers, where waiting data can be stored
 N_BUFFERS = 2
 
 DIMS = (BATCH_SIZE * N_BUFFERS * 2 + 1 + 1,  # 2 reading buffers, 2 writing buffers, 1 Info, 1 values
@@ -147,29 +159,29 @@ class ProcessPredictor:
 if __name__ == "__main__":
     if current_process().name == "MainProcess":
         mp.set_start_method("spawn", force=True)
-        print("Creating shared block")
-        shr = create_shared_block()
-        manager = mp.Manager()
-        lock = manager.Lock()
-        predictor = ProcessPredictor(shr.name, lock)
-        model_saver = ModelSaver()
+        while True:
+            with mp.Manager() as manager:
+                print("Creating shared block")
+                shr = create_shared_block()
+                lock = manager.Lock()
+                predictor = ProcessPredictor(shr.name, lock)
+                model_saver = ModelSaver()
 
-        print("Start Predictor Process")
-        predictor_process = Process(target=predictor_loop, args=(shr.name, lock))
-        predictor_process.start()
-        time.sleep(5)
+                print("Start Predictor Process")
+                predictor_process = Process(target=predictor_loop, args=(shr.name, lock))
+                predictor_process.start()
+                time.sleep(5)
 
-        for _ in range(N_ITER_EPISODE):
-            begin_time = time.time()
-            with mp.Pool(N_POOLS) as pool:
-                episodes = pool.map(run_episode_independant,
-                                    [(predictor, N_SIMULATIONS, ITER_MAX) for _ in range(N_EPISODES)])
-            examples = [x for episode in episodes for x in episode]
-            model_saver.save_episodes(examples)
-            print("Total time:", time.time() - begin_time)
+                begin_time = time.time()
+                with mp.Pool(N_POOLS) as pool:
+                    episodes = pool.map(run_episode_independant,
+                                        [(predictor, N_SIMULATIONS, ITER_MAX) for _ in range(N_EPISODES)])
+                examples = [x for episode in episodes for x in episode]
+                model_saver.save_episodes(examples)
+                print("Total time:", time.time() - begin_time)
 
-        predictor_process.terminate()
-        predictor_process.join()
-        predictor_process.close()
-        shr.close()
-        shr.unlink()
+                predictor_process.terminate()
+                predictor_process.join()
+                predictor_process.close()
+                shr.close()
+                shr.unlink()
